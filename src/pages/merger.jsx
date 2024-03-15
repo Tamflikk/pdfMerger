5ee5a6e2bd317558ba8e1c81d992d6b3a2882e5e
Tamflikk <alejandrotamayo32200@gmail.com>
2024-03-15 06:03:12 -0400

    update merger.jsx

--- src/pages/merger.jsx
+++ src/pages/merger.jsx
@@ -120,7 +120,7 @@ const Merger = () => {
             onClick={handleMergePDF}
             sx={{
               color: "white",
-              width: "30%",
+              width: "50%",
               mr: 2,
               borderColor: "white",
               "&:hover": {
@@ -137,7 +137,7 @@ const Merger = () => {
             onClick={handleAddDocuments}
             sx={{
               color: "white",
-              width: "30%",
+              width: "40%",
               borderColor: "white",
               "&:hover": {
                 borderColor: "white",
